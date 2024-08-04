2a5d1d8fac1edb3f8ce3f4b8a533c4effce242b1
Kyle Plump <kplump1395@gmail.com>
2024-08-04 10:14:47 -0400

    untested switch from long poll to observer

--- rhino.rb
+++ rhino.rb
@@ -3,51 +3,44 @@
 module Rhino
   require 'redis'
   require 'json'
+  require 'observer'
 
   class Queue
+    include Observable
+
     def initialize(name:)
       @name = name
       @redis = Redis.new(host: '127.0.0.1', port: 6379, db: 6)
     end
 
     def add(job_name:, data:)
-      @redis.set("rhino:#{@name}:#{job_name}", data.to_json)
+      changed
+      key = "rhino:#{@name}:#{job_name}"
+      @redis.set(key, data.to_json)
+      notify_observers(key)
     end
   end
 
   class Worker
-    def initialize(name:, handler:)
-      @name = name
+    def initialize(queue:, handler:)
+      @queue = queue
       @handler = handler
       @redis = Redis.new(host: '127.0.0.1', port: 6379, db: 6)
       Thread.new do
-        check_queue # immediately process queue
-
-        # this sucks
-        loop do
-          sleep 1
-          check_queue
-        end
+        @queue.add_observer(self, :process_queue)
       end
     end
 
     private
 
-    def check_queue
-      p 'queue check'
-      res = @redis.keys("rhino:#{@name}:*")
+    def process_queue(key)
+      job = @redis.keys(key)
 
-      jobs = res.map do |key|
-        data = @redis.get(key)
-
-        Ractor.new [@handler, @redis, key, data] do |handler, redis, key, data|
-          data_obj = JSON.parse(data)
-          method(handler).call(key.split(':').last, data_obj)
-          redis.del(key)
-        end
-      end
-
-      jobs.each(&:take)
+      Ractor.new [@handler, @redis, key, data] do |handler, redis, key, data|
+        data_obj = JSON.parse(data)
+        method(handler).call(key.split(':').last, data_obj)
+        redis.del(key)
+      end.take
     end
   end
 end
@@ -63,7 +56,7 @@ def handler(job_name, data)
   p "completed job #{job_name}"
 end
 
-Rhino::Worker.new(name: 'tester', handler: :handler)
+Rhino::Worker.new(queue: r, handler: :handler)
 
 sleep 8
 
